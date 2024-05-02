713abd8755fa310787369a963485f53ae3f64c81
tjglwcz <117677164+tjglwcz@users.noreply.github.com>
2024-05-02 10:13:33 +0200

    Add Footer component and make Navbar sticky

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import { Footer } from "./components/Footer";
 import { Navbar } from "./components/Navbar";
 
 function App() {
   return (
     <div className="flex flex-col">
-      <header>
+      <header className="sticky top-0 bg-background">
         <Navbar />
       </header>
+      <main className="h-screen">a</main>
+      <Footer />
     </div>
   );
 }
