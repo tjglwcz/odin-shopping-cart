2b4fc9986c0016c856b4b3c9090872d7ae21b0d8
tjglwcz <117677164+tjglwcz@users.noreply.github.com>
2024-05-02 12:42:55 +0200

    Create routing to products page

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Root } from "./routes/root";
+import { Products } from "./routes/products";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "products/",
+        element: <Products />,
+      },
+    ],
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/routes/root.tsx
@@ -0,0 +1,17 @@
+import { Navbar } from "../components/Navbar";
+import { Footer } from "../components/Footer";
+import { Outlet } from "react-router-dom";
+
+export const Root = () => {
+  return (
+    <div className="flex flex-col">
+      <header className="sticky top-0 bg-background">
+        <Navbar />
+      </header>
+      <main className="h-screen">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
+};
