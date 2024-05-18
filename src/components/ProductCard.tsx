97a26faefb5144128427cd339abbce29dde7897b
tjglwcz <117677164+tjglwcz@users.noreply.github.com>
2024-05-18 08:22:11 +0200

    Add image prop to ProductCard component

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -11,21 +11,29 @@ import { Button } from "./ui/button";
 interface productCardProps {
   name: string;
   category: string;
+  image: string;
   price: number;
 }
 
-export const ProductCard = ({ name, category, price }: productCardProps) => {
+export const ProductCard = ({
+  name,
+  category,
+  image,
+  price,
+}: productCardProps) => {
   return (
-    <Card>
+    <Card className="h-full flex flex-col justify-center">
       <CardHeader>
         <CardTitle>{name}</CardTitle>
         <CardDescription>{category}</CardDescription>
       </CardHeader>
-      <CardContent>
-        <div className="min-h-32 w-auto"></div>
+      <CardContent className="flex-1 flex flex-col justify-center">
+        <div className="flex justify-center py-8">
+          <img src={image} alt="" className="object-fit max-h-64" />
+        </div>
       </CardContent>
-      <CardFooter className="flex gap-4 items-center justify-between">
-        <p className="text-xl font-semibold">${price}</p>
+      <CardFooter className="flex items-center justify-between">
+        <p className="text-xl font-semibold">${price.toFixed(2)}</p>
         <Button>Add to cart</Button>
       </CardFooter>
     </Card>
