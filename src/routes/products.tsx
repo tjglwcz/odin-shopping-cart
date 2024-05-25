13c55b5824409c9d70d20fa2772aad0b0010cabd
tjglwcz <117677164+tjglwcz@users.noreply.github.com>
2024-05-25 13:15:38 +0200

    Add API fetching based on category

--- src/routes/products.tsx
+++ src/routes/products.tsx
@@ -1,13 +1,37 @@
 import { ProductCard } from "@/components/ProductCard";
 import { useEffect, useState } from "react";
-export const Products = () => {
+export const Products = ({ category }: { category: string }) => {
   const [products, setProducts] = useState<any[]>([]);
 
+  function fetchItems(cat: string) {
+    if (cat === "electronics") {
+      return fetch(`https://fakestoreapi.com/products/category/electronics`, {
+        mode: "cors",
+      });
+    } else if (cat === "jewelery") {
+      return fetch(`https://fakestoreapi.com/products/category/jewelery`, {
+        mode: "cors",
+      });
+    } else if (cat === `men's clothing`) {
+      return fetch(
+        `https://fakestoreapi.com/products/category/men's%20clothing`,
+        { mode: "cors" }
+      );
+    } else if (cat === `women's clothing`) {
+      return fetch(
+        `https://fakestoreapi.com/products/category/women's%20clothing`,
+        { mode: "cors" }
+      );
+    } else {
+      return fetch(`https://fakestoreapi.com/products`);
+    }
+  }
+
   useEffect(() => {
-    fetch(`https://fakestoreapi.com/products`)
+    fetchItems(category)
       .then((res) => res.json())
       .then((items) => setProducts(items));
-  }, []);
+  }, [category]);
 
   return (
     <div className="grid auto-cols-fr p-4 md:grid-cols-4 gap-4">
